f41384ede274e42e2f8b823ac4c665bc45716255
titydelor <delortity@gmail.com>
2022-08-24 21:28:42 +0100

    nouvelle fonctionalite

---
+++ src/main/java/com/softwify/libraryapp/dao/NativeJdbcAuthorDao.java
@@ -0,0 +1,137 @@
+package com.softwify.libraryapp.dao;
+
+import com.softwify.libraryapp.configuration.DataBaseConfig;
+import com.softwify.libraryapp.constants.Queries;
+import com.softwify.libraryapp.model.Author;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NativeJdbcAuthorDao implements AuthorDao {
+    private static final Logger logger = LogManager.getLogger(NativeJdbcAuthorDao.class.getSimpleName());
+
+    private final DataBaseConfig dataBaseConfig;
+
+    public NativeJdbcAuthorDao(DataBaseConfig dataBaseConfig) {
+        this.dataBaseConfig = dataBaseConfig;
+    }
+
+    public List<Author> getAll() {
+        Connection connection = null;
+        List<Author> authors = new ArrayList<>();
+
+        try {
+            connection = dataBaseConfig.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(Queries.GET_AUTHORS);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String firstname = resultSet.getString("firstname");
+                String lastname = resultSet.getString("lastname");
+                Author author = new Author(id, firstname, lastname);
+                authors.add(author);
+            }
+            dataBaseConfig.closeResultSet(resultSet);
+            dataBaseConfig.closePreparedStatement(preparedStatement);
+        } catch (SQLException | ClassNotFoundException e) {
+            logger.error("An error has occurred", e);
+        } finally {
+            dataBaseConfig.closeConnection(connection);
+        }
+        return authors;
+    }
+
+    public boolean delete(int id) {
+        Connection connection = null;
+        boolean deleted = false;
+        try {
+            connection = dataBaseConfig.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(Queries.DELETE_AUTHOR);
+            preparedStatement.setInt(1, id);
+            int affectedRows = preparedStatement.executeUpdate();
+            deleted = affectedRows == 1;
+            dataBaseConfig.closePreparedStatement(preparedStatement);
+        } catch (SQLException | ClassNotFoundException e) {
+            logger.error("An error has occurred", e);
+        } finally {
+            dataBaseConfig.closeConnection(connection);
+        }
+
+        return deleted;
+    }
+
+    public Author save(Author author) {
+        Connection connection = null;
+        Author createdAuthor = null;
+        try {
+            connection = dataBaseConfig.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(Queries.ADD_AUTHOR, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, author.getFirstName());
+            preparedStatement.setString(2, author.getLastName());
+            preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            if(resultSet.next()) {
+                int id = resultSet.getInt(1);
+                createdAuthor = new Author(id, author.getFirstName(), author.getLastName());
+            }
+            dataBaseConfig.closePreparedStatement(preparedStatement);
+        } catch (SQLException | ClassNotFoundException e) {
+            logger.error("An error has occurred", e);
+        } finally {
+            dataBaseConfig.closeConnection(connection);
+        }
+        return createdAuthor;
+    }
+
+    public boolean checkExistingAuthor(Author author) {
+        Connection connection = null;
+        boolean isAuthorExist = false;
+        try {
+            connection = dataBaseConfig.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(Queries.CHECK_IF_AUTHOR_EXIST);
+            preparedStatement.setString(1, author.getFirstName());
+            preparedStatement.setString(2, author.getLastName());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                int count = resultSet.getInt(1);
+                isAuthorExist = count > 0;
+            }
+            dataBaseConfig.closeResultSet(resultSet);
+            dataBaseConfig.closePreparedStatement(preparedStatement);
+        } catch (SQLException | ClassNotFoundException e) {
+            logger.error("An error has occurred", e);
+        } finally {
+            dataBaseConfig.closeConnection(connection);
+        }
+
+        return isAuthorExist;
+    }
+
+    public Author getByFirstNameAndLastName(String firstName, String lastName) {
+        Connection connection = null;
+        Author author = null;
+        try {
+            connection = dataBaseConfig.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(Queries.GET_AUTHOR);
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                author = new Author(id, firstName, lastName);
+            }
+            dataBaseConfig.closeResultSet(resultSet);
+            dataBaseConfig.closePreparedStatement(preparedStatement);
+        } catch (SQLException | ClassNotFoundException e) {
+            logger.error("An error has occurred", e);
+        } finally {
+            dataBaseConfig.closeConnection(connection);
+        }
+
+        return author;
+    }
+
+}
